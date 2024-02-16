88d5562ef5a563eaea73a915f655fd997f449cea
Ayush Sharma <ayush.sharma@fretron.com>
2024-02-16 17:07:25 +0530

    data class and JSON

--- 2. Data Class And JSON/dataClassAndJSON/build.gradle.kts
+++ 2. Data Class And JSON/dataClassAndJSON/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("com.google.code.gson:gson:2.8.9")
     api ("org.json:json:20170516")
 }
 
--- 2. Data Class And JSON/dataClassAndJSON/src/main/kotlin/Main.kt
+++ 2. Data Class And JSON/dataClassAndJSON/src/main/kotlin/Main.kt
@@ -1,19 +1,39 @@
+// Task:  To create a class and override its toString() method to return a stringified JSON object and
+//converting it to JSON object and then mapping it to back to class and then printing that object
+
 package org.example
 
 import org.json.JSONObject
+import com.google.gson.Gson
+import java.lang.reflect.Field
+import java.util.Objects
 
-class Vehicle(val regNo : String, val chasisNo : String, val noOfWheels : Int){
+data class Vehicle(var regNo : String?, var chasisNo : String?, var noOfWheels : Int?){
+    constructor(): this(regNo = null, chasisNo = null, noOfWheels = null)
     override fun toString(): String {
-        return "Vehicle(regNo : $regNo, chasisNo : $chasisNo, noOfWheels : $noOfWheels)"
+        val vehicleJson = JSONObject()       //we generally use this
+        vehicleJson.put("regNo",regNo)
+        vehicleJson.put("chasisNo",chasisNo)
+        vehicleJson.put("noOfWheels",noOfWheels)
+        return vehicleJson.toString()
+//        val vehicleJson = JSONObject(this)
+//        return vehicleJson.toString()
     }
 }
 
 fun main() {
-    val v = Vehicle("A123","B123",4)
-    val jsonString = JSONObject(v).toString()
-    println(jsonString)     //json string
-    val jsonObject = JSONObject(jsonString)
-    println(jsonObject)     //json object
-    val v1 = Vehicle(jsonObject.getString("regNo"),jsonObject.getString("chasisNo"),jsonObject.getInt("noOfWheels"))
-    println(v1)
+    val vehicle = Vehicle().apply {
+        this.regNo = "423432"
+        this.chasisNo = "543285"
+        this.noOfWheels = 4
+    }
+    println(vehicle)     //vehicle json string
+
+    val vehicleJson = JSONObject(vehicle)   //vehicle json object
+
+    //using gson for mapping
+    val gson = Gson()
+    val secondVehicle: Vehicle = gson.fromJson(vehicleJson.toString(), Vehicle::class.java)
+    println(secondVehicle)
+
 }
